4016a4ede64d5d04ca201ef0d1c5a19388f403a8
Taller-1-2do-Semestre <ignacio_salomon@outlook.es>
2025-09-10 00:59:47 -0300

    Tengo el cerebro al frito ahora, me puse a seguir el tutorial y creo que funcionó. Yo solo queria mandar un correo simple hace como una hora atrás.

---
+++ src/AyudantiaApoyo.java
@@ -0,0 +1,30 @@
+import java.awt.*;
+
+public class AyudantiaApoyo {
+
+    public static void AyudantiaApoyo(String[] args) {
+
+        int contador = 10;
+
+        //StdDraw.setXscale(-1,1);
+        //StdDraw.setYscale(-1,1);
+
+        //StdDraw.picture(0,0, "UCN_Logo.svg.png");
+
+        //StdDraw.setPenColor(Color.BLUE);
+
+        while (true) {
+            contador --;
+            //StdDraw.text(-0.5,0.5, "Hola : " + contador);
+            //StdDraw.show();
+            //StdDraw.pause(200);
+
+            if (contador == 0) {
+                break;
+            }
+        }
+
+
+        //StdDraw.show();
+    }
+}
